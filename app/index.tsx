d1fc9f28ae1302ad33e44957cff5d82385d1b974
Sayuri Gamage <sayuriyashodhara@gmail.com>
2025-09-03 21:53:01 +0530

    create alert for home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles"; // ✅ importing styles
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text> 
+      
+      <TouchableOpacity onPress={() => alert("you touched")}>
+        <Text>Press me</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={() => alert("you touched")}>
+        <Text>Press me - pressable</Text>
+      </Pressable>
     </View>
   );
 }
